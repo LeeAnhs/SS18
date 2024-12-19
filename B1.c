0fda4e21b1e42cedafd7517f9bf46b334aac736b
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-12-19 13:54:44 +0700

    B1.c

---
+++ B1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    struct sinhvien {
+        char name[50];
+        int age;
+        int phoneNumber;
+    };
+  struct sinhvien SV01={"LeXuanAnh",18,329325058};
+  printf("Name: %s\n", SV01.name);
+  printf("Age: %d\n", SV01.age);
+  printf("ID: %ld\n", SV01.phoneNumber);
+  return 0;
+  
+}
