8156203bbe022920879fc952de349a792ead181d
LeeAnhs <138470988+LeeAnhs@users.noreply.github.com>
2024-12-20 09:05:55 +0700

    B4.c

--- B4.c
+++ B4.c
@@ -1,29 +1,32 @@
 #include <stdio.h>
 
-int main() {
+int main(void) {
     struct sinhvien {
         int id;
         char name[50];
         int age;
-        char phoneNumber[50];
+        char phoneNumber[10];
     };
+
     struct sinhvien SV[4];
     for(int i = 0; i < 5; i++) {
-        SV[i].id = i + 1;
-        printf("Name(SV%d): ", i + 1);
-        fgets(SV[i].name, sizeof(SV[i].name), stdin);
-        printf("Age(SV%d): ", i + 1);
-        scanf("%d", &SV[i].age);
+        sinhvien[i].id = i + 1;
+        printf("Ten %d: ", i + 1);
+        fgets(sinhvien[i].name, sizeof(sinhvien[i].name), stdin);
+        printf("Age %d: ", i + 1);
+        scanf("%d", &sinhvien[i].age);
         fflush(stdin);
-        printf("SDT(SV%d):\n ", i + 1);
-        fgets(SV[i].phoneNumber, sizeof(SV[i].phoneNumber), stdin);
+        printf("SDT %d: ", i + 1);
+        fgets(sinhvien[i].phoneNumber, sizeof(sinhvien[i].phoneNumber), stdin);
         fflush(stdin);
+        printf("\n");
     }
 
     for(int i = 0; i < 5; i ++) {
-        printf("ID %d\n", SV[i].id);
-        printf("Name:%s", SV[i].name);
-        printf("Age:%d\n", SV[i].age);
-        printf("SDTSDT: %s\n", SV[i].phoneNumber);
+        printf("Person id %d\n", sinhvien[i].id);
+        printf("Name: %s", sinhvien[i].name);
+        printf("Age: %d\n", sinhvien[i].age);
+        printf("Phone number: %s\n", sinhvien[i].phoneNumber);
+        printf("\n");
     }
 }
